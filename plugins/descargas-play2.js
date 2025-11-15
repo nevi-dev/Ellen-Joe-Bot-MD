8353b117e8b866b6cc740597dba07d8473942b8d
Nevi <208419680+nevi-dev@users.noreply.github.com>
2025-11-15 14:48:42 -0400

    Update descargas-play2.js

--- plugins/descargas-play2.js
+++ plugins/descargas-play2.js
@@ -1,1 +1,18 @@
+let handler = async (m, { conn, command, usedPrefix }) => {
+let img = 'icons'
+let staff = `ᥫ᭡ *EQUIPO DE AYUDANTES* ❀
+✰ *Dueño* ${author}
+✦ *Bot: ${namebot}
+⚘ *Versión:* ${vs}
+❖ *Libreria:* ${libreria} ${baileys}
+> ✧ GitHub » https://github.com/nevi-dev
+`
+await conn.sendFile(m.chat, img, 'Ellen.jpg', staff.trim(), fkontak)
+}
+  
+handler.help = ['staff']
+handler.command = ['colaboradores', 'staff']
+handler.register = true
+handler.tags = ['main']
 
+export default handler
