29986c4c24a8e76f222ab4081fcbb6cb7905f720
Nevi <208419680+nevi-dev@users.noreply.github.com>
2026-01-08 12:32:18 -0400

    Actualizar nsfw-rule34.js

--- plugins/nsfw-rule34.js
+++ plugins/nsfw-rule34.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 
 // --- CONFIGURACIÓN DE LA API ---
 const API_BASE_URL = "https://api-causas.duckdns.org/api/v1/nsfw/descargas/rule34";
-const API_KEY = "causa-7c134894eacc45d7";
+const API_KEY = "causa-6e2da545497cfba1";
 
 const handler = async (m, { conn, args, usedPrefix, command }) => {
     // 1. Verificación de NSFW
