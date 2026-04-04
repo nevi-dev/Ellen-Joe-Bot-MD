2ea54a2bd8d41601c4fc94077b5ffd1d137b026f
Nevi <208419680+nevi-dev@users.noreply.github.com>
2026-04-04 15:00:51 -0400

    Actualizar anime-gifts.js

--- plugins/anime-gifts.js
+++ plugins/anime-gifts.js
@@ -26,7 +26,7 @@ let handler = async (m, { conn, command, usedPrefix }) => {
     let from = m.pushName || await getName(m.sender)
     let who = await getName(userId)
 
-    const apiKey = "causa-ec43262f206b3305"
+    const apiKey = "causa-ee5ee31dcfc79da4"
 
     const isNsfw = command === 'waifuh'
     const type = isNsfw ? 'nsfw' : 'sfw'
