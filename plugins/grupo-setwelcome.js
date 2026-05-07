4903762c3be0c9488dd707957c0f887b187290b0
Nevi <208419680+nevi-dev@users.noreply.github.com>
2026-05-07 01:42:44 -0400

    Update grupo-setwelcome.js

--- plugins/grupo-setwelcome.js
+++ plugins/grupo-setwelcome.js
@@ -7,9 +7,9 @@ let handler = async (m, { conn, text }) => {
   await conn.reply(m.chat, '✅ Bienvenida guardada correctamente.', m)
 }
 
-handler.command = /^setwelcome$/i // Aquí el RegExp está bien
-handler.help = ['setwelcome <texto>'] // Debe ser un Array de Strings
-handler.tags = ['admin'] // Debe ser un Array de Strings
+handler.help = ['setwelcome']
+handler.tags = ['admin']
+handler.command = ['setwelcome']
 handler.admin = true
 handler.group = true
 
