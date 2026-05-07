1ed436f3955b0130c79492b4c635180fdc24c344
Nevi <208419680+nevi-dev@users.noreply.github.com>
2026-05-07 01:25:25 -0400

    Update grupo-setwelcome.js

--- plugins/grupo-setwelcome.js
+++ plugins/grupo-setwelcome.js
@@ -7,6 +7,7 @@ let handler = async (m, { conn, text }) => {
   await conn.reply(m.chat, '✅ Bienvenida guardada correctamente.', m)
 }
 
+handler.help = ['setwelcome'];
 handler.tags = ['admin'];
 handler.command = ['setwelcome'];
 handler.admin = true
