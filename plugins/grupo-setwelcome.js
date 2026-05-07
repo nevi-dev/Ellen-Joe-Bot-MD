7c217374bdd5dd6b906fa2407ab01673e8a20ad2
Nevi <208419680+nevi-dev@users.noreply.github.com>
2026-05-07 01:45:21 -0400

    Update grupo-setwelcome.js

--- plugins/grupo-setwelcome.js
+++ plugins/grupo-setwelcome.js
@@ -1,15 +1,12 @@
 let handler = async (m, { conn, text }) => {
-  if (!text) return conn.reply(m.chat, '🦈 ¡Oye! Pon el mensaje.\nEjemplo: #setwelcome Hola @user', m)
+  if (!text) return conn.reply(m.chat, '🦈 ¡Oye! Proporciona un mensaje de bienvenida.\n\n*Variables disponibles:*\n#group (Nombre)\n#desc (Descripción)\n#members (Cantidad)\n@user (Mención)', m)
   
   if (!global.db.data.chats[m.chat]) global.db.data.chats[m.chat] = {}
   global.db.data.chats[m.chat].sWelcome = text
   
   await conn.reply(m.chat, '✅ Bienvenida guardada correctamente.', m)
 }
-
-handler.help = ['setwelcome']
-handler.tags = ['admin']
-handler.command = ['setwelcome']
+handler.command = /^setwelcome$/i
 handler.admin = true
 handler.group = true
 
